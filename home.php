7aca5f72bf2fad42eae5acda70d93b883e241bda
slv-improver <k.moret.slv@gmail.com>
2020-02-10 09:32:35 +0100

    add home.php to test db data

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php
+require 'Database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+   <meta charset="utf-8">
+   <title>Mon blog</title>
+</head>
+
+<body>
+   <div>
+      <h1>Mon blog</h1>
+      <p>En construction</p>
+
+      
+
+   </div>
+</body>
+
+</html>
