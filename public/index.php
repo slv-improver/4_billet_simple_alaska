c23bc677a4c2010cbc9368d7505038700397ea9c
slv-improver <k.moret.slv@gmail.com>
2020-02-12 13:15:06 +0100

    add class Router & move public/index.php content to its function run()

--- public/index.php
+++ public/index.php
@@ -3,16 +3,5 @@
 require '../config/dev.php';
 require '../vendor/autoload.php';
 
-try {
-   if (isset($_GET['route'])) {
-      if ($_GET['route'] === 'chapter') {
-         require '../templates/single.php';
-      } else {
-         echo 'Page inconnue';
-      }
-   } else {
-      require '../templates/home.php';
-   }
-} catch (Exception $e) {
-   echo 'Erreur';
-}
+$router = new \App\config\Router();
+$router->run();
