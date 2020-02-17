46f3c8b9bc9936fd5b031fd03a415410f46ac83d
Algor <k.moret.slv@gmail.com>
2020-02-17 14:11:08 +0100

    move tinymce.js to tinymce/init.js & add fr_FR language

--- templates/base.php
+++ templates/base.php
@@ -4,7 +4,7 @@
 <head>
    <meta charset="UTF-8" />
    <script src="https://cdn.tiny.cloud/1/byx5g5l289zh3erindewgepf8uvsnwxuk6xnc5064r8nli6f/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-   <script src='../public/js/tinymce.js'></script>
+   <script src='js/tinymce/init.js'></script>
    <title><?= $title ?></title>
 </head>
 
