9d0901b31bbf2dd79e27aa1f9b15d21629191d17
slv-improver <k.moret.slv@gmail.com>
2020-02-19 10:44:39 +0100

    add containsUpperCase() and containsLowerCase() on Constraint.php for password

--- src/constraint/Constraint.php
+++ src/constraint/Constraint.php
@@ -22,4 +22,16 @@ class Constraint
 			return "<p>Le champ \"$name\" doit contenir au maximum $maxSize caractères</p>";
 		}
 	}
+	public function containsUpperCase($value)
+	{
+		if (!preg_match("#[A-Z]#", $value)) {
+			return "<p>Le mot de passe doit contenir au moins une lettre majuscule</p>";
+		}
+	}
+	public function containsLowerCase($value)
+	{
+		if (!preg_match("#[a-z]#", $value)) {
+			return "<p>Le mot de passe doit contenir au moins une lettre minuscule</p>";
+		}
+	}
 }
