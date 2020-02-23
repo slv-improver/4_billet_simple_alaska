33c220c6ea5cdd4ce901833a6c0fbfece0bcbce3
slv-improver <k.moret.slv@gmail.com>
2020-02-23 09:07:30 +0100

    add bootstrap cdn css and js on base.php

--- templates/base.php
+++ templates/base.php
@@ -3,6 +3,7 @@
 
 <head>
 	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<script src="https://cdn.tiny.cloud/1/byx5g5l289zh3erindewgepf8uvsnwxuk6xnc5064r8nli6f/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 	<script src='js/tinymce/init.js'></script>
 	<title><?= $title ?></title>
@@ -14,4 +15,5 @@
 	</div>
 </body>
 
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </html>
